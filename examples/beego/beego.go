7b6960976ba7304e623a19d87871defafd05cfa3
cloud <1017371914@qq.com>
2016-12-26 23:08:19 +0800

    完善消息处理

--- examples/beego/beego.go
+++ examples/beego/beego.go
@@ -24,7 +24,43 @@ func hello(ctx *context.Context) {
 	server.SetMessageHandler(func(msg message.MixMessage) *message.Reply {
 
 		//回复消息：演示回复用户发送的消息
-		text := message.NewText(msg.Content)
+
+		text := message.NewText("我已收到您的消息会尽快处理")
+
+		switch msg.MsgType {
+		//文本消息
+		case message.MsgTypeText:
+		//do something
+			fmt.Print("文本消息")
+			text = message.NewArticle("那个嫁给了穷人的女同学", "爆漫画", "http://zxpic.gtimg.com/infonew/0/wechat_pics_-10190065.static/640", "http://v.juhe.cn/weixin/redirect?wid=wechat_20161226038185")
+		//图片消息
+		case message.MsgTypeImage:
+		//do something
+			fmt.Print("图片消息")
+		//语音消息
+		case message.MsgTypeVoice:
+		//do something
+			fmt.Print("语音消息")
+		//视频消息
+		case message.MsgTypeVideo:
+		//do something
+			fmt.Print("视频消息")
+		//小视频消息
+		case message.MsgTypeShortVideo:
+		//do something
+			fmt.Print("小视频消息")
+		//地理位置消息
+		case message.MsgTypeLocation:
+		//do something
+			fmt.Print("地理位置消息")
+		//链接消息
+		case message.MsgTypeLink:
+		//do something
+			fmt.Print("连接消息")
+		//事件推送消息
+		case message.MsgTypeEvent:
+			fmt.Print("事件推送消息")
+		}
 		return &message.Reply{message.MsgTypeText, text}
 	})
 
